b4178b3d5c5327518cd854e1b69e9288e63b6fa5
Rabindra Dhakal <contact@qaidvoid.dev>
2025-02-25 20:00:11 +0545

    perf(run): improve run performance for cached binary

--- soar-cli/src/run.rs
+++ soar-cli/src/run.rs
@@ -24,7 +24,7 @@ pub async fn run_package(
     pkg_id: Option<&str>,
 ) -> SoarResult<()> {
     let state = AppState::new();
-    let repo_db = state.repo_db().await?;
+    let cache_bin = state.config().get_cache_path()?.join("bin");
 
     let package_name = &command[0];
     let args = if command.len() > 1 {
@@ -33,33 +33,34 @@ pub async fn run_package(
         &[]
     };
 
-    let mut builder = PackageQueryBuilder::new(repo_db.clone())
-        .where_and("pkg_name", FilterCondition::Eq(package_name.clone()));
-
-    if let Some(repo_name) = repo_name {
-        builder = builder.where_and("repo_name", FilterCondition::Eq(repo_name.to_string()));
-    }
-
-    if let Some(pkg_id) = pkg_id {
-        builder = builder.where_and("pkg_id", FilterCondition::Eq(pkg_id.to_string()));
-    }
-
-    let packages: Vec<Package> = builder.load()?.items;
-
-    let package = match packages.len() {
-        0 => return Err(SoarError::PackageNotFound(package_name.clone())),
-        1 => packages.into_iter().next(),
-        _ if yes => packages.into_iter().next(),
-        _ => select_package_interactively(packages, &package_name)?,
-    }
-    .unwrap();
-
-    let cache_bin = state.config().get_cache_path()?.join("bin");
-    fs::create_dir_all(&cache_bin)
-        .with_context(|| format!("creating directory {}", cache_bin.display()))?;
-
-    let output_path = cache_bin.join(&package.pkg_name);
+    let output_path = cache_bin.join(package_name);
     if !output_path.exists() {
+        let repo_db = state.repo_db().await?;
+
+        let mut builder = PackageQueryBuilder::new(repo_db.clone())
+            .where_and("pkg_name", FilterCondition::Eq(package_name.clone()));
+
+        if let Some(repo_name) = repo_name {
+            builder = builder.where_and("repo_name", FilterCondition::Eq(repo_name.to_string()));
+        }
+
+        if let Some(pkg_id) = pkg_id {
+            builder = builder.where_and("pkg_id", FilterCondition::Eq(pkg_id.to_string()));
+        }
+
+        let packages: Vec<Package> = builder.load()?.items;
+
+        let package = match packages.len() {
+            0 => return Err(SoarError::PackageNotFound(package_name.clone())),
+            1 => packages.into_iter().next(),
+            _ if yes => packages.into_iter().next(),
+            _ => select_package_interactively(packages, &package_name)?,
+        }
+        .unwrap();
+
+        fs::create_dir_all(&cache_bin)
+            .with_context(|| format!("creating directory {}", cache_bin.display()))?;
+
         let progress_bar = create_progress_bar();
         let progress_callback = Arc::new(move |state| {
             progress::handle_progress(state, &progress_bar);
